24a27c2ee3d121970f3e43829fa52f381a699e8e
mingcaixiao <mirmingcaixiao@gmail.com>
2024-04-25 21:12:10 +0800

    add 63 and 64

---
+++ 63_uniquePathsWithObstacles.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
+        for(int i = 0; i < m; ++i){
+            if(obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+                break; //continue cannot arrive
+            }
+            else dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; ++i){
+            if(obstacleGrid[0][i] == 1){
+                dp[0][i] = 0;
+                break;
+            }
+            else dp[0][i] = 1;
+        }
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+
+int main(){
+    // std::vector<std::vector<int>> data{{0,0,0}, {0,1,0}, {0,0,0}};
+    std::vector<std::vector<int>> data{{1, 0}};
+    Solution s;
+    std::cout << s.uniquePathsWithObstacles(data) << std::endl;
+    return 1;
+}
